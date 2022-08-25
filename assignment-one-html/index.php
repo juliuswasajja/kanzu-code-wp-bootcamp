eb612b2c5b33dc0d51ec335a251c33c8cc9e7fd2
Julius Wasajja <juliuswasajja@icloud.com>
2022-08-25 07:57:13 +0300

    php-part-1

--- assignment-one-html/index.php
+++ assignment-one-html/index.php
@@ -9,7 +9,7 @@
     <link rel="stylesheet" href="assets/fontawesome/webfonts/fa-solid-900.ttf">
 
     <title>To Do List</title>
-<body>
+    <body>
     <header>
         
     </header>
@@ -22,16 +22,84 @@
         
         <div class="row" id="form-container">
             <div class="col-9">
-                <form id="my-todos-form">
-                    <input id="add-new-to-do-item" type="text" placeholder="Add new...">
+                <form id="my-todos-form" method="POST" >
+                    <input id="add-new-to-do-item" name='todoitem' type="text" placeholder="Add new...">
             </div>
+            
 
             <div class="col-3">
                 <!-- <span class="calendaricon" style="color:#006CFF ;"><i class="fa-regular fa-calendar-days"></i></span> -->
                     <!-- <input type="date"> -->
                     <input class="form-button" type="submit" value="Add">
                 </form>
+                
             </div>
+
+            
+
+            
+        </div>
+
+
+        <div class='row'>
+
+        <p>
+            <?php
+                echo 'final test';
+
+                print_r($_REQUEST);
+                //file_put_contents("post.log", print_r($_POST['todoitem'], true));
+                file_put_contents("postreq.log", print_r($_REQUEST, true));
+
+
+            ?>
+        </p>
+
+
+            <p>
+          
+                
+                
+            <?php 
+            
+            if (array_key_exists("todoitem",$_POST))
+            {
+            echo "Key exists!";
+            }
+            else
+            {
+            echo "Key does not exist!";
+            }
+
+
+            echo 'This div is for php only ';
+            //var_dump($_POST['todoitem']);
+            var_dump($_REQUEST);
+            var_dump($_GET);
+            
+            
+            ?></p>
+        <?php
+            if ( isset($_POST['todoitem'])){
+            //print_r($_POST['todoitem']);
+            //
+            //var_dump($_POST['todoitem']);
+
+            $item_name = $_POST['todoitem'];
+
+            ?> <p> <?php
+
+            echo $item_name;
+
+            ?> </p>
+            <?php
+            }
+            // else{
+            //     echo 'not set';
+            // }
+
+            ?>
+
         </div>
         
         <div class="row" id="filtersection">
